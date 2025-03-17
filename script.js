0ccdbd7df5467b9b873be7cc4b9a2edb99e9903b
Matheus <ra-23371012-2@alunos.unicesumar.edu.br>
2025-03-17 20:35:00 -0300

    aula

---
+++ script.js
@@ -0,0 +1,25 @@
+/*
+    console.log("hello word no body");
+    var primeiroTipo;
+    let segundoTipo;
+    const terceiroTipo = 10;
+
+    primeiroTipo = 2;
+    segundoTipo =3;
+    console.log(primeiroTipo+segundoTipo);
+    console.log(segundoTipo*terceiroTipo);
+
+    var nome;
+    var idade;
+
+    nome = "Teste estou testando";
+    idade = 33;
+
+    console.log(nome);
+    nome = idade;
+    console.log(nome);*/
+
+    function hello(){
+        document.getElementById('hello').innerHTML= "Peido"
+    }
+    
